07cc5c9967696176c27f838358517c3d13db8e9a
xboxfanj <xboxlover360@gmail.com>
2016-01-16 18:45:48 +0100

    msm8996-common: Clean up kernel cmdline
     * Console is overwritten by the bootloader
     * CONFIG_DEBUG_USER is disabled, so no need to keep user_debug
       and the same goes for msm_rtb, which depends on CONFIG_MSM_RTB
    
    Change-Id: I9a30f32f7dc91cf82f2516bf6a54d7d62f1c00f1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,7 +46,7 @@ TARGET_USES_64_BIT_BINDER := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff
 BOARD_KERNEL_CMDLINE += androidboot.configfs=true
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_CMDLINE += loop.max_part=7
