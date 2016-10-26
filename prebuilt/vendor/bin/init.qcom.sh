ee0b537bfd21858cef8d5d7830e4a006a7f334cc
Bruno Martins <bgcngm@gmail.com>
2016-10-27 00:06:20 +0100

    msm8996-common: Migrate to shared touchpanel nodes
     * This allows us to easily support devices
       assembled with different touchscreens
    
    Change-Id: I45a2aec2dd6c7d28f4536a69654cf3bba3934744

--- prebuilt/vendor/bin/init.qcom.sh
+++ prebuilt/vendor/bin/init.qcom.sh
@@ -27,6 +27,9 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
+# Set shared touchpanel nodes ownership (these are proc_symlinks to the real sysfs nodes)
+chown -LR system.system /proc/touchpanel
+
 target=`getprop ro.board.platform`
 low_ram=`getprop ro.config.low_ram`
 if [ -f /sys/devices/soc0/soc_id ]; then
