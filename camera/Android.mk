39fc463e370f7162b76e5356210539c5a4c36355
LuK1337 <priv.luk@gmail.com>
2017-01-26 00:48:19 +0100

    msm8996: Add libc hax for camera
    * Fixes camera on Le Max 2
    * Taken from https://github.com/LineageOS/android_device_xiaomi_kenzo/commit/27583097b2da370eac965f7237a769ce496882db
    
    Change-Id: Ifa8aa8254bad31f42854b888e739643f210c1534

--- camera/Android.mk
+++ camera/Android.mk
@@ -23,3 +23,18 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    bionic/bionic_time_conversions.cpp \
+    bionic/pthread_cond.cpp
+
+LOCAL_SHARED_LIBRARIES := libc
+
+LOCAL_MODULE := qcamera-daemon_shim
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_32_BIT_ONLY := true
+
+include $(BUILD_SHARED_LIBRARY)
