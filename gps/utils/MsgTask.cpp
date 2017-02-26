8611a632567cf962413d0342146fab09620fed4d
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-02-26 09:24:19 +0100

    msm8996: gps: update from op3
    Change-Id: I19e9954e81d2df08c58ef1ebb81df3e1455a98f8

--- gps/utils/MsgTask.cpp
+++ gps/utils/MsgTask.cpp
@@ -29,12 +29,11 @@
 #define LOG_NDDEBUG 0
 #define LOG_TAG "LocSvc_MsgTask"
 
-#include <cutils/sched_policy.h>
 #include <unistd.h>
 #include <MsgTask.h>
 #include <msg_q.h>
-#include <log_util.h>
 #include <loc_log.h>
+#include <platform_lib_includes.h>
 
 static void LocMsgDestroy(void* msg) {
     delete (LocMsg*)msg;
@@ -63,9 +62,9 @@ MsgTask::~MsgTask() {
 }
 
 void MsgTask::destroy() {
+    LocThread* thread = mThread;
     msg_q_unblock((void*)mQ);
-    if (mThread) {
-        LocThread* thread = mThread;
+    if (thread) {
         mThread = NULL;
         delete thread;
     } else {
@@ -79,7 +78,7 @@ void MsgTask::sendMsg(const LocMsg* msg) const {
 
 void MsgTask::prerun() {
     // make sure we do not run in background scheduling group
-    set_sched_policy(gettid(), SP_FOREGROUND);
+     platform_lib_abstraction_set_sched_policy(platform_lib_abstraction_gettid(), PLA_SP_FOREGROUND);
 }
 
 bool MsgTask::run() {
