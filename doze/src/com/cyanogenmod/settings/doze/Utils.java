d97121d3b6f413e5e1ca817164b49c8b73388eb0
Bruno Martins <bgcngm@gmail.com>
2017-05-13 14:02:28 +0300

    msm8996-common: Doze: Refactor
    * Fix Ambient Display's switchbar on/off text.
    * Get rid of unnecessary receiver and all its related code,
      permissions and meta-data.
    * Keep the service running only if Doze is enabled system-wide along
      with at least one of the additional Ambient Display features.
    
    Change-Id: I76913cf375d0462557af2b9833ad136e733ef0f9

--- doze/src/com/cyanogenmod/settings/doze/Utils.java
+++ doze/src/com/cyanogenmod/settings/doze/Utils.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2015 The CyanogenMod Project
+ *               2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +19,6 @@ package com.cyanogenmod.settings.doze;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.UserHandle;
 import android.support.v7.preference.PreferenceManager;
 import android.provider.Settings;
@@ -33,12 +33,9 @@ public final class Utils {
 
     private static final String DOZE_INTENT = "com.android.systemui.doze.pulse";
 
-    protected static final String AMBIENT_DISPLAY_KEY = "doze_enabled";
     protected static final String GESTURE_PICK_UP_KEY = "gesture_pick_up";
     protected static final String GESTURE_HAND_WAVE_KEY = "gesture_hand_wave";
     protected static final String GESTURE_POCKET_KEY = "gesture_pocket";
-
-    public static final Uri DOZE_ENABLED_URI = Settings.Secure.getUriFor(DOZE_ENABLED);
 
     protected static void startService(Context context) {
         if (DEBUG) Log.d(TAG, "Starting service");
@@ -52,20 +49,22 @@ public final class Utils {
                 UserHandle.CURRENT);
     }
 
+    protected static void checkDozeService(Context context) {
+        if (isDozeEnabled(context) && sensorsEnabled(context)) {
+            startService(context);
+        } else {
+            stopService(context);
+        }
+    }
+
     protected static boolean isDozeEnabled(Context context) {
         return Settings.Secure.getInt(context.getContentResolver(),
                 DOZE_ENABLED, 1) != 0;
     }
 
     protected static boolean enableDoze(boolean enable, Context context) {
-        boolean dozeEnabled = Settings.Secure.putInt(context.getContentResolver(),
-                              DOZE_ENABLED, enable ? 1 : 0);
-        if (enable) {
-            startService(context);
-        } else {
-            stopService(context);
-        }
-        return dozeEnabled;
+        return Settings.Secure.putInt(context.getContentResolver(),
+                DOZE_ENABLED, enable ? 1 : 0);
     }
 
     protected static void launchDozePulse(Context context) {
