3df4957b34c1a25fa6f7b0bfedc8743efd0f67ad
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-24 20:55:55 -0700

    msm8996: Add netutils-wrapper impl
    Change-Id: Id6f168c7b8a30ee6dc368b12ca0521ad173f24f4

--- msm8996.mk
+++ msm8996.mk
@@ -250,6 +250,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# Netutils
+PRODUCT_PACKAGES += \
+    netutils-wrapper-1.0
+
 # OMX
 PRODUCT_PACKAGES += \
     libmm-omxcore \
