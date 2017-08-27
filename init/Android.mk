75f4705bbcf8663fcaaf40c4c13ade64418d6733
Simon Shields <simon@lineageos.org>
2017-08-27 13:47:35 +0200

    msm8996: init: use libbase for log headers
    Change-Id: I37257fe72642b2744f4a022c5495ff411c939634

--- init/Android.mk
+++ init/Android.mk
@@ -20,6 +20,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
+LOCAL_STATIC_LIBRARIES := libbase
 LOCAL_SRC_FILES := init_leeco_msm8996.cpp
 LOCAL_MODULE := libinit_leeco_msm8996
 
