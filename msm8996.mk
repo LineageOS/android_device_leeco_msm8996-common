1e73c53f4eb191948b20449fab8f10496dfbab71
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-08-28 00:48:29 +0200

    msm8996: add mediaserver seccomp policy, use media profiles from marlin
    Change-Id: I1ba004016b2c7e6827fddfa5c740584c673e11b9

--- msm8996.mk
+++ msm8996.mk
@@ -246,7 +246,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
@@ -286,6 +286,10 @@ PRODUCT_PACKAGES += \
     rmnetcli \
     libxml2 \
     libprotobuf-cpp-full
+
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # Sensors
 PRODUCT_PACKAGES += \
