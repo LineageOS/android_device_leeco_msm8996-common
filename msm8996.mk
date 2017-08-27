a3d43285ea545712a7ba84644315a2be23cff8ba
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-08-27 23:47:57 +0200

    msm8996: disable additional boot jars
    Change-Id: Ibab104242959c07d6c4c1bd173572edf5266dd08

--- msm8996.mk
+++ msm8996.mk
@@ -97,8 +97,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.sdcardfs=true
 
 # Alipay / WeChat
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+#PRODUCT_BOOT_JARS += \
+#    org.ifaa.android.manager
 
 # Audio
 PRODUCT_PACKAGES += \
