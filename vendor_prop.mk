194ff2d02654d5bd499fd2234079f3c4bbc3daa0
Satya Krishna Pindiproli <satyak@codeaurora.org>
2017-09-13 17:10:38 +0530

    msm8996-common: add system property to set AudioFlinger  client heap size
    Playback of high sample rate multichannel clips fails when
    repeat mode is set to repeat one. This is because the default
    heap size allocated for AudioFlinger clients is 4MB which is insufficient.
    
    Set the system property ro.af.client_heap_size_kbyte to 7KB. This property
    is read in AudioFlinger and multiplied by 1024, so effectively the allocated
    heap size becomes 7MB which is sufficient to play such clips in repeat mode.
    
    CRs-Fixed: 2108978
    Change-Id: I72f4bb9892465afb311bb269545a5fe926696b02

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -13,6 +13,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.fluence.speaker=true \
     persist.vendor.audio.fluence.voicecall=true \
     persist.vendor.audio.fluence.voicerec=false \
+    ro.af.client_heap_size_kbyte=7168 \
     ro.config.media_vol_steps=25 \
     ro.config.vc_call_vol_steps=7 \
     ro.vendor.audio.sdk.fluencetype=fluence \
