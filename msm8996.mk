ec27365bf8f3d40e7bc25f20ab41572fc38a175a
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-10-03 17:01:21 +0200

    msm8996: add gatekeeper service
    Change-Id: I596d3cdf757d87894887ce4de8b164c2ab845fc9

--- msm8996.mk
+++ msm8996.mk
@@ -212,7 +212,8 @@ PRODUCT_PACKAGES += \
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0-impl
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
 
 # GPS
 PRODUCT_PACKAGES += \
