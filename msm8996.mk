f8cfd7fdb0d03f2291fb819809c275c58eaa76a0
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-10-03 17:02:23 +0200

    msm8996: build bt service
    Change-Id: Icc7998e7b9093ce4cbe20ef8833d9e9cafd1b741

--- msm8996.mk
+++ msm8996.mk
@@ -146,7 +146,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
     libbt-vendor
 
 # Camera
