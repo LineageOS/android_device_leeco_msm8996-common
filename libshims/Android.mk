5e193a9a84a46bcb3912c67fa1f3f43413c0b256
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-10-04 21:35:54 +0200

    msm8996: remove get_process_name shim
    Change-Id: I91058fc19c3b2d13e71339facb72c5a9105644d7

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -42,14 +42,3 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_32_BIT_ONLY := true
 
 include $(BUILD_SHARED_LIBRARY)
-
-
-# libshims_get_process_name
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := get_process_name.c
-
-LOCAL_MODULE := libshims_get_process_name
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
