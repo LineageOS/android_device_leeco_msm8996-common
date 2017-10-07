8a20d0718342173ce3ae27c7bbc696b187a86fbc
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-10-07 19:38:33 +0200

    msm8996: add camera hidl, add camera shim
    Change-Id: I05241b18ab512b8b9015827743f30e11be02413d

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -20,9 +20,19 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := camera_shim.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libgui \
+    libui
+
+LOCAL_C_INCLUDES := \
+    frameworks/native/include
+
 LOCAL_MODULE := libshims_camera
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+LOCAL_MULTILIB := 32
 
 include $(BUILD_SHARED_LIBRARY)
 
@@ -39,6 +49,6 @@ LOCAL_SHARED_LIBRARIES := libc
 LOCAL_MODULE := libshims_qcamera-daemon
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_32_BIT_ONLY := true
+LOCAL_MULTILIB := 32
 
 include $(BUILD_SHARED_LIBRARY)
