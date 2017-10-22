b1b13a4bcdd64d8beab2d3fe50d4165448b8e276
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-10-22 18:37:25 +0200

    msm8996: update config.fs
    Change-Id: Ida9b7b35acfe8e725e6f3f5725ec285dcef2fca7

--- config.fs
+++ config.fs
@@ -1,46 +1,10 @@
 [AID_QTI_DIAG]
 value:2901
 
-[system/bin/qmuxd]
-mode: 0700
-user: AID_RADIO
-group: AID_SHELL
-caps: BLOCK_SUSPEND
-
-[system/bin/mm-qcamera-daemon]
-mode: 0700
-user: AID_CAMERA
-group: AID_SHELL
-caps: SYS_NICE
-
-[system/bin/pm-service]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
 [system/bin/cnss-daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/bin/imsdatadaemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/bin/ims_rtp_daemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_RADIO
-caps: NET_BIND_SERVICE
-
-[system/vendor/bin/slim_daemon]
-mode: 0755
-user: AID_SYSTEM
-group: AID_GPS
 caps: NET_BIND_SERVICE
 
 [system/bin/wcnss_filter]
@@ -48,3 +12,33 @@ mode: 0755
 user: AID_BLUETOOTH
 group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND
+
+[system/vendor/bin/cnd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
+
+[system/vendor/bin/imsdatadaemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/ims_rtp_daemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/pm-service]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/slim_daemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_GPS
+caps: NET_BIND_SERVICE
