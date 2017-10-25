bf91bf0f996758f4fa93c8d1043e86ed13b4dd82
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-10-25 19:28:18 +0200

    msm8996: remove not needed vendor.qti.hardware.camera.device@1.0
    Change-Id: If7eb5d80669fb24053358920c353ceb978b1d12c

--- msm8996.mk
+++ msm8996.mk
@@ -153,10 +153,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@3.2-impl \
-    vendor.qti.hardware.camera.device@1.0 \
     libshims_camera \
     libshims_qcamera-daemon \
     Snap
+#    vendor.qti.hardware.camera.device@1.0 \
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
