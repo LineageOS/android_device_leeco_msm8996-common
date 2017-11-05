5ce812960dde24cc4bb08dccce2a9478659f8950
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-11-05 11:42:29 +0100

    msm8996: move Lineage specific overlays to overlay-lineage
    Change-Id: I8d7d33117754b85b3be7c65feb4d1366aa993b08

--- msm8996.mk
+++ msm8996.mk
@@ -15,7 +15,9 @@
 #
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+#    $(LOCAL_PATH)/overlay-lineage
 
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
