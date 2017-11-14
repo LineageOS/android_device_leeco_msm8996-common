5e6caec01cf9a643a994bff7a2f98979917e8581
Michael Bestas <mkbestas@lineageos.org>
2017-11-14 17:22:37 +0200

    camera: Cleanup Obsolete LOCAL_PRELINK_MODULE.
    Change-Id: I2b48ea97aef4dcce31f1354c021d409f68d8dc65

--- camera/QCamera2/stack/mm-camera-interface/Android.mk
+++ camera/QCamera2/stack/mm-camera-interface/Android.mk
@@ -53,7 +53,7 @@ LOCAL_CFLAGS += -Wall -Wextra -Werror
 LOCAL_SRC_FILES := $(MM_CAM_FILES)
 
 LOCAL_MODULE           := libmmcamera_interface
-LOCAL_PRELINK_MODULE   := false
+
 LOCAL_SHARED_LIBRARIES := libdl libcutils liblog libutils
 LOCAL_HEADER_LIBRARIES := libhardware_headers
 LOCAL_MODULE_TAGS := optional
