e52466ef2ce7856c5ae4fcddb9d9ea76ee5a2a24
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-01-05 12:42:02 +0100

    msm8996: camera: remove metadata shim
    Update header to match
    https://github.com/LineageOS/android_frameworks_av/commit/cb5576b5784a817c6976040efc2b64d317d4a69e
    
    Change-Id: Ibdc35e5dcd65b525b646d14accae105748170afe

--- include/camera/CameraMetadata.h
+++ include/camera/CameraMetadata.h
@@ -222,7 +222,7 @@ class CameraMetadata: public Parcelable {
 
   private:
     camera_metadata_t *mBuffer;
-    volatile bool      mReserved __attribute__ ((unused));
+    volatile char      mReserved[3] __attribute__ ((unused));
     mutable bool       mLocked;
 
     /**
