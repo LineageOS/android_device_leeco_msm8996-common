e194ef7e9650f67bc6e47e21f3fc21c4881c471b
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-01-06 21:04:06 +0100

    msm8996: opt-out of los charger, add healthd
    Change-Id: Iae37cc9031481fb354bb96c03a3adbba1690af25

--- msm8996.mk
+++ msm8996.mk
@@ -193,6 +193,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/FOSSConfig.xml:system/etc/FOSSConfig.xml
 
+# Healthd
+PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-convert \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-service
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
