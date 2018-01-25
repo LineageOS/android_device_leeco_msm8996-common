705482fc3dda6d42e4a633db7b1f99ba4c6451ab
Michael Bestas <mkbestas@lineageos.org>
2018-01-25 23:13:40 +0200

    msm8996-common: Drop audiod
    * It's now deprecated
    
    Change-Id: I00c8bdc09da4ba39508314ec8feba67d60e4fb04

--- msm8996.mk
+++ msm8996.mk
@@ -87,7 +87,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    audiod \
     audio.a2dp.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
