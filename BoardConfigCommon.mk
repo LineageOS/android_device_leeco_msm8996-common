d83ac16740d65c268c8f91692d559f78eb4bbd04
Gabriel Noguerales <gabronogue@gmail.com>
2018-03-03 12:04:58 +0100

    msm8996: add hvdcp_opti (leeco quick charge)
    Change-Id: I3a0b999f23f214d9f5632a21eb3701e4ec15756b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,6 +115,7 @@ BOARD_QTI_CAMERA_32BIT_ONLY := true
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
+# Before enabling lineage charger you have to fix it!
 WITH_LINEAGE_CHARGER := false
 
 # Init
