f2acecdcc1b13275614fbacd0a7c907551b78fcf
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-04-08 12:12:48 +0200

    msm8996: add thermal, usb and vibrator services
    Thermal sources:
    https://source.codeaurora.org/quic/la/platform/hardware/qcom/thermal/tree/?h=LA.UM.6.5.r1-06700-8x96.0
    
    Change-Id: I082e62936a32166b7b17efaf3c189cf2df16f28c

--- msm8996.mk
+++ msm8996.mk
@@ -357,13 +357,20 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Thermal
+# Tetheroffload
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl
+    ipacm \
+    IPACM_cfg.xml
 
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
+
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.msm8996
 
 # USB
 PRODUCT_PACKAGES += \
@@ -372,7 +379,8 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 # VNDK-SP:
 PRODUCT_PACKAGES += \
@@ -395,10 +403,6 @@ PRODUCT_PACKAGES += \
     wificond \
     wpa_supplicant \
     wpa_supplicant.conf
-
-PRODUCT_PACKAGES += \
-    ipacm \
-    IPACM_cfg.xml
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
