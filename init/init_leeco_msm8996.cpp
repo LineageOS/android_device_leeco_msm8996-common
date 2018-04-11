e795f0da93ff996b1f7cc2431dd34331d7ac1638
Marc Bourgoin <themard85@gmail.com>
2018-04-11 09:56:45 +0200

    x2: add le_x2_india (LEX821) to init
    Change-Id: I310494c231378e69871d77e7ddb12182f192b374

--- init/init_leeco_msm8996.cpp
+++ init/init_leeco_msm8996.cpp
@@ -129,6 +129,15 @@ void init_target_properties()
             property_set("persist.nfc.smartcard.config", "SIM1,SIM2,eSE1");
             unknownDevice = 0;
         }
+        else if (!strncmp(device.c_str(), "le_x2_india", 11)) {
+            // This is LEX821
+            property_override_dual("ro.product.model", "ro.vendor.product.model", "LEX821");
+            // Dual SIM
+            property_set("persist.radio.multisim.config", "dsds");
+            // NFC
+            property_set("persist.nfc.smartcard.config", "SIM1,SIM2,eSE1");
+            unknownDevice = 0;
+        }
         else if (!strncmp(device.c_str(), "le_x2", 5)) {
             // This is LEX820
             property_override_dual("ro.product.model", "ro.vendor.product.model", "LEX820");
