a6ba3f189decf8b37ad8b103e631334ff71ac4c7
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-04-28 18:48:00 +0200

    msm8996: update gps from LA.UM.6.5.r1-07400-8x96.0
    Change-Id: Ic9d4ea1e69f0f9e4ccf56917fe96696876a83a42

--- msm8996.mk
+++ msm8996.mk
@@ -223,7 +223,7 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
-# GPS
+# GPS / Location
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl-qti \
     android.hardware.gnss@1.0-service-qti \
