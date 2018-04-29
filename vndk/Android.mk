06f282d0b7c2c23a049cca26be10b323de1d861f
Vinay Verma <vvinay@codeaurora.org>
2018-04-30 00:24:13 +0530

    msm8996-common: Disable vndk-sp creation for VNDK enabled targets.
    Change-Id: Idf17c7ddeab922c231fc3e9bb786c45dfa54684b

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(BOARD_VNDK_VERSION),)
+$(warning ************* BOARD VNDK is not enabled - compiling vndk-sp ***************************)
 LOCAL_PATH := $(call my-dir)
 
 include $(LOCAL_PATH)/vndk-sp-libs.mk
@@ -51,3 +53,4 @@ LOCAL_REQUIRED_MODULES := \
 include $(BUILD_PHONY_PACKAGE)
 
 vndk_sp_dir :=
+endif
