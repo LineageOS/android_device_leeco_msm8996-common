96d815f3b9f56c6c4058c01cfe2e933eb5a4db75
Marc Bourgoin <themard85@gmail.com>
2018-07-07 01:17:16 -0600

    msm8996: add ims-ext-common
    * imx-ext-common is part of CAF IMS telephony
    * provide description for IMS settings in overlay
    * also remove duplicate Healthd entries
    * also move HIDL to be organized alphabetically
    
    Change-Id: I8701d02a50a6228eca113dcf8e8b4ca039d556d9

--- msm8996.mk
+++ msm8996.mk
@@ -187,18 +187,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/FOSSConfig.xml:system/etc/FOSSConfig.xml
 
-# Healthd
-PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-convert \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service
-
-# HIDL
-PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.manager@1.0 \
-    android.hidl.manager@1.0-java
-
 # Doze mode
 PRODUCT_PACKAGES += \
     Doze
@@ -245,6 +233,16 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl \
     android.hardware.health@1.0-convert \
     android.hardware.health@1.0-service
+
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0-java
+
+# IMS
+PRODUCT_PACKAGES += \
+    ims-ext-common
 
 # IRQ
 PRODUCT_COPY_FILES += \
