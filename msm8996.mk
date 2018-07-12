3ae32ba412139b2beecd6a94208662deb6706be3
Marc Bourgoin <themard85@gmail.com>
2018-07-12 02:41:49 -0600

    msm8996: kang widevine from marlin
    * current implementation does not work, so directly kang the files
    le_x2:/ # ls vendor/bin/hw/android.hardware.drm@1.0-service.widevine
    ls: vendor/bin/hw/android.hardware.drm@1.0-service.widevine: No such file or directory
    
    * also remove depreciated widevine drm files
    
    Change-Id: Ief85100ec9bdab4d275f82b2c4ea133f019ee325

--- msm8996.mk
+++ msm8996.mk
@@ -184,8 +184,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.0-service.widevine
+    android.hardware.drm@1.0-service
 
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
