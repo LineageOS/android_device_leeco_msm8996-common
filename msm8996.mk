738d84dec43c0d7fa3177c5e581b06cd61f55e1a
shivatejapeddi <shivatejapeddi@gmail.com>
2018-07-18 23:18:17 +0530

    msm8996-common: build the VNDK blobs requested by camera
    Change-Id: I14d308014c9118627d5bf8519bd5b38870c838b9

--- msm8996.mk
+++ msm8996.mk
@@ -130,8 +130,11 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.device@1.0.so \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
+    camera.device@1.0-impl \
     camera.msm8996 \
     libshims_camera \
     libshims_qcamera-daemon \
--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -1,9 +1,11 @@
 VNDK_SP_LIBRARIES := \
+    android.hardware.camera.device@1.0 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4 \
     android.hardware.graphics.allocator@2.0 \
     android.hardware.graphics.mapper@2.0 \
     android.hardware.graphics.common@1.0 \
     android.hardware.renderscript@1.0 \
-    android.hidl.base@1.0 \
     android.hidl.memory@1.0 \
     libRSCpuRef \
     libRSDriver \
@@ -25,4 +27,39 @@ VNDK_SP_LIBRARIES := \
     liblzma \
     libpng \
     libunwind \
-    libutils
+    libutils \
+    libz
+
+EXTRA_VENDOR_LIBRARIES := \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
+    android.hardware.camera.device@1.0 \
+    android.hardware.camera.device@3.2 \
+    android.hardware.camera.device@3.3 \
+    android.hardware.camera.provider@2.4 \
+    libcamera_metadata \
+    libfmq \
+    android.hardware.camera.common@1.0 \
+    android.hardware.configstore@1.0 \
+    android.hardware.configstore-utils \
+    android.hidl.allocator@1.0 \
+    android.hidl.memory@1.0 \
+    libbinder \
+    libui
+
+HW_VENDOR_LIBRARIES := \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
+    android.hardware.camera.device@1.0 \
+    android.hardware.camera.device@3.2 \
+    android.hardware.camera.device@3.3 \
+    android.hardware.camera.provider@2.4 \
+    libcamera_metadata \
+    libfmq \
+    android.hardware.camera.common@1.0 \
+    android.hardware.configstore@1.0 \
+    android.hardware.configstore-utils \
+    android.hidl.allocator@1.0 \
+    android.hidl.memory@1.0 \
+    libbinder \
+    libui
