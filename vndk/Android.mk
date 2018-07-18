64961c3e802f47cf60f6ec19a9779d00571d8d98
shivatejapeddi <shivatejapeddi@gmail.com>
2018-07-18 23:18:17 +0530

    msm8996-common: build the VNDK blobs requested by camera
    Change-Id: I14d308014c9118627d5bf8519bd5b38870c838b9

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -34,9 +34,16 @@ endef
 
 $(foreach lib,$(VNDK_SP_LIBRARIES),\
     $(eval $(call define-vndk-sp-lib,$(lib))))
+$(foreach lib,$(EXTRA_VENDOR_LIBRARIES),\
+    $(eval $(call define-vndk-lib,$(lib),vndk-ext-gen,,true)))
+$(foreach lib,$(HW_VENDOR_LIBRARIES),\
+    $(eval $(call define-vndk-lib,$(lib),vndk-hw-gen,hw,true)))
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := vndk-sp
 LOCAL_MODULE_TAGS := optional
-LOCAL_REQUIRED_MODULES := $(addsuffix .vndk-sp-gen,$(VNDK_SP_LIBRARIES))
+LOCAL_REQUIRED_MODULES := \
+    $(addsuffix .vndk-sp-gen,$(VNDK_SP_LIBRARIES)) \
+    $(addsuffix .vndk-ext-gen,$(EXTRA_VENDOR_LIBRARIES)) \
+    $(addsuffix .vndk-hw-gen,$(HW_VENDOR_LIBRARIES))
 include $(BUILD_PHONY_PACKAGE)
