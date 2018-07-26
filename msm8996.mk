50ab67350e1dd43d2636a3473945bd562fb7c2f0
Lakshman Chaluvaraju <lchalu@codeaurora.org>
2018-07-26 15:04:37 +0530

    MSM8996: Move Audio HAL from 2.0 to 4.0
    - Moving Audio HAL from 2.0 to 4.0.
    
    Change-Id: I623d92bae679a89b0af4863d315f34edaf30019f

--- msm8996.mk
+++ msm8996.mk
@@ -78,9 +78,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@4.0-impl \
     audio.a2dp.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
