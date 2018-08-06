17c40d45099943f619f60c9133856fca130276c9
ThEMarD <themard85@gmail.com>
2018-08-06 06:16:37 -0600

    msm8996: update device tree to current blob locations
    for config.fs:
    * sort alpabetically
    * system -> vendor
    * add android.hardware.bluetooth@1.0-service-qti and imsrcsd
    * slim_daemon: AID_SYSTEM -> AID_GPS
    
    for init.qcom.rc:
    * energy-awareness, mm-pp-dpps, mdtpd, dpmQmiMgr, wcnss_filter, netmgrd, cnss-daemon and sensors.qcom system/vendor -> vendor
    
    for sepolicy/file_contexts:
    * ATFWD-daemon, energy-awareness and wcnss_filter system -> vendor
    * remove btnvtool (we no longer have it)
    
    Change-Id: I1950ad4f290220fdac93ed3dd0a6d0af87180d7d

--- config.fs
+++ config.fs
@@ -10,44 +10,56 @@ value:2951
 [AID_RFS_SHARED]
 value:2952
 
-[system/bin/cnss-daemon]
+[vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND NET_ADMIN
+
+[vendor/bin/cnss-daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/bin/wcnss_filter]
-mode: 0755
-user: AID_BLUETOOTH
-group: AID_BLUETOOTH
-caps: BLOCK_SUSPEND
-
-[system/vendor/bin/cnd]
+[vendor/bin/cnd]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
 
-[system/vendor/bin/imsdatadaemon]
+[vendor/bin/imsdatadaemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/vendor/bin/ims_rtp_daemon]
+[vendor/bin/ims_rtp_daemon]
 mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
-[system/vendor/bin/pm-service]
+[vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: WAKE_ALARM
+
+[vendor/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[system/vendor/bin/slim_daemon]
+[vendor/bin/slim_daemon]
 mode: 0755
-user: AID_SYSTEM
+user:  AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE
+
+[vendor/bin/wcnss_filter]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND
