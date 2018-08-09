bc443a1f7129d52906061e73f6d6313968622c7e
ThEMarD <themard85@gmail.com>
2018-08-09 03:20:00 -0600

    msm8996: Bring config.fs more in line with CAF
    * add firmware, bt_firmware, persist and dsp
    
    Change-Id: I8a0bd9423ee4b97cf45a3395195b08c546648dd6

--- config.fs
+++ config.fs
@@ -63,3 +63,27 @@ mode: 0755
 user: AID_BLUETOOTH
 group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND
+
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[bt_firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[dsp/]
+mode: 0771
+user: AID_MEDIA
+group: AID_MEDIA
+caps: 0
