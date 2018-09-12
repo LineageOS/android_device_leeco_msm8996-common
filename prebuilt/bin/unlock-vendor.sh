35a802c4ee10ec2db109e6bb4c2dd40ba95f68f3
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-09-12 19:03:49 +0200

    msm8996: reread the partition table after creating vendor partition
    Change-Id: If5b4c7709e7f24deb6a7222775e00ebae24e397f

--- prebuilt/bin/unlock-vendor.sh
+++ prebuilt/bin/unlock-vendor.sh
@@ -1,4 +1,29 @@
 #!/sbin/sh
+# Copyright (c) 2018, The LineageOS Project
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the LineageOS Project nor
+#       the names of its contributors may be used to endorse or promote
+#       products derived from this software without specific prior written
+#       permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # Unlock vendor partition
 #
@@ -17,7 +42,7 @@ safeRunCommand() {
 }
 
 if [ ! -e "$VENDOR" ]; then
-    echo "Vendor partition does not exist"
+    echo "Vendor partition does not exist. Trying to create one..."
 
     # Change typecode to '8300 Linux filesystem'
     command="/tmp/sgdisk --typecode=34:8300 $BLOCKDEV"
@@ -26,6 +51,10 @@ if [ ! -e "$VENDOR" ]; then
     # Change name to 'vendor'
     command="/tmp/sgdisk --change-name=34:vendor $BLOCKDEV"
     safeRunCommand $command
+
+    # Reread the partition table
+    /sbin/blockdev --rereadpt $BLOCKDEV
+    echo "Done!"
 else
     echo "Found vendor partiton. Good!"
 fi
