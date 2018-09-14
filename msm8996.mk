fa50b3ddf17994cf4f980a00915324843393703f
ThEMarD <themard85@gmail.com>
2018-09-13 22:02:22 -0600

    msm8996: enable treble OMX media prop
    * not defining the prop is the same as false (according to this commit https://github.com/LineageOS/android_frameworks_av/commit/9c47c97ecac581d66b6febafd156618247e86742 )
    * also organize vendor.prop alphabetically
    * improve omx in msm8996.mk
    
    Change-Id: Ie097551631e30f41f10a53cd460459e965315930

--- msm8996.mk
+++ msm8996.mk
@@ -272,6 +272,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
     libextmedia_jni \
+    libmm-omxcore \
     libOmxAacEnc \
     libOmxAmrEnc \
     libOmxCore \
@@ -279,7 +280,8 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    android.hardware.media.omx@1.0-impl
 
 # Power
 PRODUCT_PACKAGES += \
