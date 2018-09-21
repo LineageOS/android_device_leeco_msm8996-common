e7714eff16a4ca7ed1ad146e89e7f357d1a24f4a
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-09-21 14:35:55 +0200

    msm8996: zl0: update deunify for P
    Change-Id: Ife9528da62f70d1f1cff235bfb0e31b9a20afe3f

--- prebuilt/bin/deunify.sh
+++ prebuilt/bin/deunify.sh
@@ -10,15 +10,21 @@ echo "DEVINFO: ${DEVINFO}"
 
 case "$DEVINFO" in
   le_zl0*)
-    # move firmware
+    # Move firmware
     mv -f /vendor/firmware/zl0/* /vendor/firmware/
 
-    # remove nfc perms
+    # Remove NFC perms
     rm -f /vendor/etc/permissions/android.hardware.nfc*
     rm -f /vendor/etc/permissions/com.android.nfc*
 
-    # remove nfc service
+    # Remove NFC services
     rm -f /vendor/etc/init/android.hardware.nfc*
+    rm -f /vendor/etc/init/vendor.nxp.hardware.nfc*
+
+    # Remove SmartcardService
+    rm -f /system/etc/permissions/org.simalliance.openmobileapi.xml
+    rm -f /system/framework/org.simalliance.openmobileapi.jar
+    rm -rf /vendor/app/SmartcardService
     ;;
   *)
     echo "Nothing to do!"
