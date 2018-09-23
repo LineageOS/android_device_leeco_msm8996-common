e02327939079310b192d90b6c28a11823b372db2
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-09-23 10:09:11 +0200

    msm8996: update qmi, radio, ims
    Change-Id: I42e8a39b07e0ef900640a036a6f159bb5003bf19

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -7,6 +7,7 @@ VNDK_SP_LIBRARIES := \
     android.hardware.graphics.mapper@2.1 \
     android.hardware.graphics.common@1.0 \
     android.hardware.graphics.common@1.1 \
+    android.hardware.radio.config@1.0 \
     android.hardware.renderscript@1.0 \
     android.hidl.memory@1.0 \
     libRSCpuRef \
