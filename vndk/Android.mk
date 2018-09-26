93bdb92905156c7da555a54f392004a7095944f0
SagarMakhar <sagarmakhar@gmail.com>
2018-09-26 04:18:33 +0000

    msm8996: use new logic for vndk-sp generation
    reference:
    https://android.googlesource.com/device/google/marlin/+/android-9.0.0_r8/vndk/Android.mk
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    
    Change-Id: I6893b06ac5932b41370ab9064c7693c45eb6f91e

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -2,7 +2,15 @@ ifeq ($(BOARD_VNDK_VERSION),)
 $(warning ************* BOARD VNDK is not enabled - compiling vndk-sp ***************************)
 LOCAL_PATH := $(call my-dir)
 
-include $(LOCAL_PATH)/vndk-sp-libs.mk
+ifndef BOARD_VNDK_VERSION
+
+VNDK_SP_LIBRARIES := \
+    libdexfile \
+    libartbase \
+    libziparchive
+
+install_in_hw_dir := \
+   android.hidl.memory@1.0-impl
 
 vndk_sp_dir := vndk-sp-$(PLATFORM_VNDK_VERSION)
 
@@ -10,7 +18,7 @@ define define-vndk-sp-lib
 include $$(CLEAR_VARS)
 LOCAL_MODULE := $1.vndk-sp-gen
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_PREBUILT_MODULE_FILE := $$(TARGET_OUT_INTERMEDIATE_LIBRARIES)/$1.so
+LOCAL_PREBUILT_MODULE_FILE := $$(call intermediates-dir-for,SHARED_LIBRARIES,$1,,,,)/$1.so
 LOCAL_STRIP_MODULE := false
 LOCAL_MULTILIB := first
 LOCAL_MODULE_TAGS := optional
@@ -24,7 +32,7 @@ ifneq ($$(TARGET_TRANSLATE_2ND_ARCH),true)
 include $$(CLEAR_VARS)
 LOCAL_MODULE := $1.vndk-sp-gen
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_PREBUILT_MODULE_FILE := $$($$(TARGET_2ND_ARCH_VAR_PREFIX)TARGET_OUT_INTERMEDIATE_LIBRARIES)/$1.so
+LOCAL_PREBUILT_MODULE_FILE := $$(call intermediates-dir-for,SHARED_LIBRARIES,$1,,,$$(TARGET_2ND_ARCH_VAR_PREFIX),)/$1.so
 LOCAL_STRIP_MODULE := false
 LOCAL_MULTILIB := 32
 LOCAL_MODULE_TAGS := optional
@@ -36,14 +44,25 @@ endif # TARGET_TRANSLATE_2ND_ARCH is not true
 endif # TARGET_2ND_ARCH is not empty
 endef
 
+# Add VNDK-SP libs to the list if they are missing
+$(foreach lib,$(VNDK_SAMEPROCESS_LIBRARIES),\
+    $(if $(filter $(lib),$(VNDK_SP_LIBRARIES)),,\
+    $(eval VNDK_SP_LIBRARIES += $(lib))))
+
+# Remove libz from the VNDK-SP list (b/73296261)
+VNDK_SP_LIBRARIES := $(filter-out libz,$(VNDK_SP_LIBRARIES))
+
 $(foreach lib,$(VNDK_SP_LIBRARIES),\
     $(eval $(call define-vndk-sp-lib,$(lib))))
 
+install_in_hw_dir :=
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := vndk-sp
+LOCAL_MODULE_OWNER := google
 LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES := $(addsuffix .vndk-sp-gen,$(VNDK_SP_LIBRARIES))
 include $(BUILD_PHONY_PACKAGE)
-
 vndk_sp_dir :=
+endif
 endif
