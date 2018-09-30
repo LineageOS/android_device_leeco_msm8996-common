c56ffb2014e6e49b07856bba5766ecd5bec414fc
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-09-30 17:54:44 +0200

    msm8996: add thermal service
    Change-Id: Ie877fde9fd1cab29070ce61932a4c57a0b9bd812

--- msm8996.mk
+++ msm8996.mk
@@ -342,6 +342,11 @@ PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
 # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.msm8996
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8996.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996.conf
 
