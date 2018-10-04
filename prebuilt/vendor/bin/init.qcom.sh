6ef71698235be60c2a2bd2c62208ad814b70cf51
shivatejapeddi <shivatejapeddi@gmail.com>
2018-10-03 23:07:32 -0600

    [TEMP] msm8996: volte hax
    * works with modified ims.apk to allow VoLTE to work after reboot
    
    Change-Id: I0714f612f56e6be0d79a3dca5e7ffae9a4b4689c

--- prebuilt/vendor/bin/init.qcom.sh
+++ prebuilt/vendor/bin/init.qcom.sh
@@ -411,3 +411,11 @@ case "$buildvariant" in
         echo "4 4 1 4" > /proc/sys/kernel/printk
         ;;
 esac
+
+# VoLTE haxx
+settings_file_global="/data/system/users/0/settings_global.xml"
+multi_sim_data_call_hits=$(grep -q -c "multi_sim_data_call\" value=\"-1\"" $settings_file_global)
+if [ $multi_sim_data_call_hits == 0 ]; then
+    sed -i 's/"multi_sim_data_call" value="[0-9]"/"multi_sim_data_call" value="-1"/g' $settings_file_global
+    restorecon $settings_file_global
+fi
