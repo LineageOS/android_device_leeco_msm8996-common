0261acd51e709b8d7a4c982c85fb2b22bed8b815
shivatejapeddi <shivatejapeddi@gmail.com>
2018-10-11 04:34:44 +0000

    msm8996: Use stock EUI offline charging animation
    * Thanks to @CrisBalGreece for extracting the required files from EUI ROM
    
    Change-Id: Ib1d86d76ecc161366bf7c7b7909260732c1d02b4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -116,6 +116,7 @@ BOARD_QTI_CAMERA_32BIT_ONLY := true
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
+BOARD_HEALTHD_CUSTOM_CHARGER_RES := $(PLATFORM_PATH)/charger/images
 # Before enabling lineage charger you have to fix it!
 WITH_LINEAGE_CHARGER := false
 
