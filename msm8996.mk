fac6b7f4680c7bb059d725b2e28264dc02a1b47e
shivatejapeddi <shivatejapeddi@gmail.com>
2018-10-11 04:34:44 +0000

    msm8996: Use stock EUI offline charging animation
    * Thanks to @CrisBalGreece for extracting the required files from EUI ROM
    
    Change-Id: Ib1d86d76ecc161366bf7c7b7909260732c1d02b4

--- msm8996.mk
+++ msm8996.mk
@@ -206,7 +206,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl \
     android.hardware.health@1.0-convert \
-    android.hardware.health@1.0-service
+    android.hardware.health@1.0-service \
+    charger_res_images
 
 # HIDL
 PRODUCT_PACKAGES += \
