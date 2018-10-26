ddae5921264eaed228132b34b3e27719bca7d1f0
andr68rus <ashtmb@gmail.com>
2018-10-26 09:04:48 +0300

    releasetools.py: mount/unmount system for correct run deunify.sh
    Change-Id: Ifb014b280518eb6d01e697a3705f3ec973241d99

--- releasetools.py
+++ releasetools.py
@@ -30,8 +30,10 @@ def FullOTA_InstallBegin(info):
   return
 
 def FullOTA_InstallEnd(info):
+  info.script.Mount("/system")
   info.script.Mount("/vendor")
   RunCustomScript(info, "deunify.sh", "")
+  info.script.Unmount("/system")
   info.script.Unmount("/vendor")
   return
 
