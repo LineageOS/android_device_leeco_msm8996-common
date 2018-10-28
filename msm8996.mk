51a2b092ac2061e7fbb553565214e2cc12f0b866
andr68rus <ashtmb@gmail.com>
2018-10-28 19:56:35 +0300

    msm8996-common: add privapp-permissions for letvremote
    Change-Id: Ib214fb37f6ed0fadb8f7df55f6dfb17813f93828

--- msm8996.mk
+++ msm8996.mk
@@ -67,7 +67,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
+    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
+    $(LOCAL_PATH)/configs/privapp-permissions-letv.xml:system/etc/permissions/privapp-permissions-letv.xml
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
