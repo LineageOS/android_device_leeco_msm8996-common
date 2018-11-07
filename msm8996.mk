bd82954b74e35f33058054f6fa51640d9305424e
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    msm8996-common: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- msm8996.mk
+++ msm8996.mk
@@ -23,7 +23,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := framework-res
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Init
 PRODUCT_COPY_FILES += \
