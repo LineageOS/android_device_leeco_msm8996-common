e978d02639fcdfb6c7ef483e6eefc43da08ba3cd
Marc Bourgoin <themard85@gmail.com>
2018-11-30 00:04:06 -0700

    msm8996: decommonize thermal-engine.conf
    * when generating new configs, zl1/0 and x2 produce different results
    
    Change-Id: Idbba2489c78179a514030bec63da2ed0dc0d213d

--- msm8996.mk
+++ msm8996.mk
@@ -353,9 +353,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.msm8996
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic \
