286fe75d24a742fc7f6ac857fc44c9d2e5c9d4a7
s4704 <igorkud00@gmail.com>
2018-12-22 07:58:45 +0300

    BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -48,6 +48,7 @@ TARGET_USES_64_BIT_BINDER := true
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff
 BOARD_KERNEL_CMDLINE += androidboot.configfs=true
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_PAGESIZE := 4096
