42f8325997bc9820c6332e048536fecfaa135015
Artem Borisov <dedsa2002@gmail.com>
2018-12-24 15:25:51 +0400

    msm8996-common: Add QCOM's WFD implementation
     * Sadly, out-of-the-box Miracast support was killed in P, so
       we need this to keep Wi-Fi display feature working.
    
    Change-Id: Id680d918b64c0e3733251db45992ec477b6fbdfb
    Signed-off-by: shivatejapeddi <shivatejapeddi@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -405,6 +405,14 @@ PRODUCT_COPY_FILES += \
     kernel/leeco/msm8996/drivers/staging/qcacld-2.0/firmware_bin/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libaacwrapper \
+    libnl
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # Model is set via init library
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
     ro.product.model
