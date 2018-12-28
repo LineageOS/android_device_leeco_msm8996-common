e4c79bbaba509d2ba2f293083fa11541da7361e4
Suresh Koleti <skoleti@codeaurora.org>
2018-12-28 14:44:41 +0530

    msm8996-common: IMS: decouple ims-ext-common from boot jars
    -- Include ims_ext_common permission xml.
    
    Change-Id: Ide3ecae57cc1909180c8d8f323aa31620e900888
    CRs-Fixed: 2373721

--- msm8996.mk
+++ msm8996.mk
@@ -374,6 +374,7 @@ PRODUCT_PACKAGES += \
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common_system \
+    ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     telephony-ext
