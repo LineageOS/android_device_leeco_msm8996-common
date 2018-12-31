9494b25c048f6c91f70af36f6774a5423d40fbd9
Marc Bourgoin <themard85@gmail.com>
2018-12-31 01:09:36 -0700

    msm8996-common: kang & update blobs from daisy
    * CNE, DPM, Peripherlal-manager, QMI, Raido & Radio IMS
    * from daisy 9/PKQ1.180917.001/V10.0.1.0.PDLMIFJ
    
      for manifest:
    * add android.hardware.broadcastradio, android.hardware.secure_element, vendor.qti.hardware.data.connection, vendor.qti.hardware.data.latency, vendor.qti.esepowermanager and vendor.qti.ims.callinfo
    * android.hardware.radio.depreciated -> android.hardware.radio.config
    * com.qualcomm.qti.imscmservice 1.0 -> 2.0
    * com.qualcomm.qti.uceservice 1.0 -> 2.0
    * vendor.qti.hardware.radio.uim 1.0 -> 1.1
    * fq'ed all Radio/ims entries
    
      for msm8996.mk:
    * removed duplicate libsensorndkbridge under GPS and leave the one under sensors
    * added android.hardware.secure_element@1.0
    
      for vendor_prop:
    * added vendor.rild.libpath
    
    Change-Id: I722787c02587a8aa6a86a56904eecad8ad9e35e3

--- init/init_leeco_msm8996.cpp
+++ init/init_leeco_msm8996.cpp
@@ -92,7 +92,7 @@ void init_target_properties()
             property_set("persist.data.iwlan.enable", "false");
             // Dual SIM
             property_set("persist.radio.multisim.config", "dsds");
-            property_set("ro.telephony.default_network", "10,10");
+            property_set("ro.telephony.default_network", "22,22");
             // Power profile
             property_set("ro.power_profile.override", "power_profile_zl0");
             unknownDevice = 0;
@@ -117,7 +117,7 @@ void init_target_properties()
             property_set("persist.data.iwlan.enable", "false");
             // Dual SIM
             property_set("persist.radio.multisim.config", "dsds");
-            property_set("ro.telephony.default_network", "10,10");
+            property_set("ro.telephony.default_network", "22,22");
             // NFC
             property_set("persist.nfc.smartcard.config", "SIM1,SIM2,eSE1");
             unknownDevice = 0;
@@ -129,7 +129,7 @@ void init_target_properties()
             property_override_dual("ro.product.name", "ro.vendor.product.name", "LeMax2_WW");
             // Dual SIM
             property_set("persist.radio.multisim.config", "dsds");
-            property_set("ro.telephony.default_network", "10,10");
+            property_set("ro.telephony.default_network", "22,22");
             unknownDevice = 0;
         }
         else if (!strncmp(device.c_str(), "le_x2_india", 11)) {
@@ -139,7 +139,7 @@ void init_target_properties()
             property_override_dual("ro.product.name", "ro.vendor.product.name", "LeMax2_WW");
             // Dual SIM
             property_set("persist.radio.multisim.config", "dsds");
-            property_set("ro.telephony.default_network", "10,10");
+            property_set("ro.telephony.default_network", "22,22");
             unknownDevice = 0;
         }
         else if (!strncmp(device.c_str(), "le_x2", 5)) {
@@ -149,7 +149,7 @@ void init_target_properties()
             property_override_dual("ro.product.name", "ro.vendor.product.name", "LeMax2_WW");
             // Dual SIM
             property_set("persist.radio.multisim.config", "dsds");
-            property_set("ro.telephony.default_network", "10,10");
+            property_set("ro.telephony.default_network", "22,22");
             unknownDevice = 0;
         }
     }
