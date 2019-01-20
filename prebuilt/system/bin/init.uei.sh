46b87bd4daa07ed0447e2939a98bdd30cb6e8459
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2019-01-20 02:23:20 +0000

    msm8996-common: implement binderized consumerir HAL + bridge
    Since LeEco doesn't use any driver blobs for consumerir and instead
    uses a quickset SDK, we need a service that acts as a bridge to the
    quickset SDK for translating calls from our consumerir  HAL,  which
    we named "ConsumerirTransmitter".
    
    How this works is our consumerir HAL receives an IR command,  which
    it  sends  to the ConsumerirTransmitter that translates it  to  the
    quickset SDK.
    
    Change-Id: I9e7eebf7071c06ec1e91cf67171b5aca7ae005d2

---
+++ prebuilt/system/bin/init.uei.sh
@@ -0,0 +1,17 @@
+#!/system/bin/sh
+
+if [ ! -f /data/data/com.uei.quicksetsdk.letv/files/Settings ]; then
+
+	# make the required directories
+	mkdir -p /data/data/com.uei.quicksetsdk.letv/files
+
+	# Copy Settings file
+	cp /system/etc/UEISettings /data/data/com.uei.quicksetsdk.letv/files/Settings
+
+	# set ownership
+	UEIUSER=$(stat -c '%U' /data/data/com.uei.quicksetsdk.letv/)
+
+	if [ -n "$UEIUSER" ]; then
+		chown -R "$UEIUSER":"$UEIUSER" /data/data/com.uei.quicksetsdk.letv/
+	fi
+fi
