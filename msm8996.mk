95e41218503e1b242027dc2846d2a9d3e33298e5
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2019-01-20 02:23:20 +0000

    msm8996-common: implement binderized consumerir HAL + bridge
    Since LeEco doesn't use any driver blobs for consumerir and instead
    uses a quickset SDK, we need a service that acts as a bridge to the
    quickset SDK for translating calls from our consumerir  HAL,  which
    we named "ConsumerirTransmitter".
    
    How this works is our consumerir HAL receives an IR command,  which
    it  sends  to the ConsumerirTransmitter that translates it  to  the
    quickset SDK.
    
    Change-Id: I9e7eebf7071c06ec1e91cf67171b5aca7ae005d2

--- msm8996.mk
+++ msm8996.mk
@@ -143,6 +143,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcnefeatureconfig
 
+# Consumer IR
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-service.leeco_8996 \
+    ConsumerirTransmitter
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
