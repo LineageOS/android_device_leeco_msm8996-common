266246a6e07f413c4e0facc96f34b71e50bfc5e6
Bruno Martins <bgcngm@gmail.com>
2019-01-21 09:40:51 +0000

    msm8996-common: Migrate to livedisplay 2.0
    Change-Id: Icb3b6187db1ce56e796f345ad31bcb69bb45be09

--- msm8996.mk
+++ msm8996.mk
@@ -241,9 +241,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.leeco_8996
 
-# LiveDisplay native
+# LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
