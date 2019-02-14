94ebc954ada1bc29da4be6fb9b477204064f3857
Akash Mondal <a98mondal@gmail.com>
2019-02-14 15:40:46 +0000

    msm8996-common: build keystore from source
    Signed-off-by: Akash Mondal <a98mondal@gmail.com>
    Change-Id: Ie273d58ac3ca0b2ead76a97f98751afb2a4801bc

--- msm8996.mk
+++ msm8996.mk
@@ -241,6 +241,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Keystore
+PRODUCT_PACKAGES += \
+    keystore.msm8996
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.leeco_8996
