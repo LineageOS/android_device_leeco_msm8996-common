1c8da145c47d2ef30e4ce08b94aeb05833dbec2e
ThE_MarD <themard85@gmail.com>
2019-04-15 19:36:08 -0600

    msm8996-common: update DRM-related blobs from OP3
    * from OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1903310200
    * also extract TWRP required decryption blobs
    * alao correct sha1sum of a pinned media blob
    
    Change-Id: I7378c7925a42545200e270a6f5855baa2e8cb299

--- extract-files.sh
+++ extract-files.sh
@@ -70,6 +70,10 @@ function blob_fixup() {
 	vendor/lib/libaudcal.so | vendor/lib64/libaudcal.so)
 		sed -i -e 's|\/data\/vendor\/misc\/audio\/acdbdata\/delta\/|\/data\/vendor\/audio\/acdbdata\/delta\/\x00\x00\x00\x00\x00|g' "${2}"
 		;;
+
+	# use /sbin instead of /system/bin for TWRP
+	recovery/root/sbin/qseecomd)
+		sed -i 's|/system/bin/linker64|/sbin/linker64\x0\x0\x0\x0\x0\x0|g' "${2}"
 	esac
 }
 
@@ -77,7 +81,12 @@ function blob_fixup() {
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
-        "${KANG}" --section "${SECTION}"
+	"${KANG}" --section "${SECTION}"
+
+if [ -s "${MY_DIR}/proprietary-files-twrp.txt" ]; then
+	extract "${MY_DIR}/proprietary-files-twrp.txt" "${SRC}" \
+		"${KANG}" --section "${SECTION}"
+fi
 
 if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
