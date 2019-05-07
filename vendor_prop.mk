fe276eac0d19c2e0802dde7bd886b7027858827e
ThE_MarD <themard85@gmail.com>
2019-05-07 09:51:38 -0600

    msm8996-common: Update vt_cam_interface prop
    Change-Id: Idc774bea88d991e0c50e1f44d3ef17dc2be51d57

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -123,9 +123,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
    persist.dbg.volte_avail_ovr=1 \
    persist.dbg.vt_avail_ovr=1 \
    persist.dbg.wfc_avail_ovr=1 \
-   persist.radio.VT_CAM_INTERFACE=2 \
    persist.radio.VT_ENABLE=1 \
-   persist.radio.VT_HYBRID_ENABLE=1
+   persist.radio.VT_HYBRID_ENABLE=1 \
+   persist.vendor.qti.telephony.vt_cam_interface=2
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
