858cd259f552a67b2a57866999583b1a8bc62d8c
mosimchah <mosimchah@gmail.com>
2019-07-26 19:39:57 -0400

    msm8996: commonize thermal-engine.conf
    - The updated thermal engine from x2 works great on zl1/zl0 so let's commonize
    - Also fix some typos in the x2 thermal engine
    
    commit dc7131e9c9f2686cd1d70243cc02a66d9261eca7
    Author: shivatejapeddi <shivatejapeddi@gmail.com>
    Date:   Sun Jul 21 07:28:11 2019 +0000
    
        x2: refine thermal-engine.conf for pie
    
        * this results in better performance stability while keeping temperstures below 80c
    
        Change-Id: Ib33eff14f0d524fc5e845875e50fac758b7ea649
    
    commit aa2846e2e6baf48160fdfaca81096a5b1109eeba
    Author: shivatejapeddi <shivatejapeddi@gmail.com>
    Date:   Thu May 9 23:24:23 2019 -0600
    
        x2: update thermal-engine config for pie thermals
    
        Change-Id: I3bd0397471ae6ea0763794493702e925759918f9
    
    commit f2f0fb39999b27f61bc25bbdea5eabe87a472338
    Author: shivatejapeddi <shivatejapeddi@gmail.com>
    Date:   Wed Feb 20 10:07:18 2019 -0700
    
        x2: update thermal-engine.conf for better balancing
    
        Change-Id: I3c433b85c577eeb277199252e472a27dc8859ca5
    
    commit 75a492810ddf8fc735078dedcd6ea0688aba85c6
    Author: Marc Bourgoin <themard85@gmail.com>
    Date:   Sat Jan 19 08:18:03 2019 -0700
    
        x2: scale thermal-engine.conf LCD brightness values properly
    
        * the old brightness scale was to 4096 which is from marshmallow. We now scale to 255
    
        Change-Id: Ic2027783b2239283bc48936ba02ea0abf1e1034a
    
    commit 653a248afa5967192a04b1a26001d867af85e883
    Author: Marc Bourgoin <themard85@gmail.com>
    Date:   Fri Jan 4 06:20:00 2019 -0700
    
        x2: disable debug from thermal-engine.conf
    
        * this will prevent it from spamming logcat
    
        Change-Id: Ib3532c5677d53a3ad3a7908be10d4f26609ce105
    
    commit 3fabdac15bf71d1fe74322ca0d08c72533328113
    Author: Marc Bourgoin <themard85@gmail.com>
    Date:   Mon Dec 3 00:14:36 2018 -0700
    
        x2: import EUI ROM 30s x820 thermal-engine.conf
    
        * from LeEco/LeMax2_CN/le_x2:6.0.1/FEXCNFN6003009092S/298
    
        Change-Id: Ifca16cc32ed4e286ae4b1553976c05182afa657f
    
    commit 180b6a6de5c7dd8f51597839d8bbcf98bba85930
    Author: Abhishek Aggarwal <warabhishek@gmail.com>
    Date:   Fri Nov 30 00:01:12 2018 -0700
    
        x2: Generate thermal-engine.conf
    
        * adb shell thermal-engine <E2><80><93>o > thermal-engine.conf
    
        Change-Id: I2845e7b8bf8ea2e8aac64bb12813a320de6cebcd
    
    Change-Id: I32c0f063a2eb8de150e9aa7fc1b062363bdd63a6

--- msm8996.mk
+++ msm8996.mk
@@ -354,6 +354,9 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.msm8996
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
