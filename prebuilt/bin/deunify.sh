c13296a23f1c91451c936998e1392a03a17090de
Marc Bourgoin <themard85@gmail.com>
2019-08-22 20:43:09 -0700

    msm8996-common: update deunify for zl0 NFC cleanup
    * smartcard service was dropped as we are using AOSP NFC stack
    * remove some extra NFC related files
    
    Change-Id: I2f82abdbe129d3b816b6382e85f67a4ef97c026c

--- prebuilt/bin/deunify.sh
+++ prebuilt/bin/deunify.sh
@@ -13,18 +13,19 @@ case "$DEVINFO" in
     # Move firmware
     mv -f /vendor/firmware/zl0/* /vendor/firmware/
 
+    # Remove NFC configs
+    rm -f /system/etc/libnfc-nci.conf
+    rm -f /vendor/etc/libnfc-nxp_RF.conf
+    rm -f /vendor/etc/libnfc-nxp.conf
+
     # Remove NFC perms
     rm -f /vendor/etc/permissions/android.hardware.nfc*
     rm -f /vendor/etc/permissions/com.android.nfc*
+    rm -f /vendor/etc/permissions/com.nxp.mifare.xml
 
     # Remove NFC services
     rm -f /vendor/etc/init/android.hardware.nfc*
     rm -f /vendor/etc/init/vendor.nxp.hardware.nfc*
-
-    # Remove SmartcardService
-    rm -f /system/etc/permissions/org.simalliance.openmobileapi.xml
-    rm -f /system/framework/org.simalliance.openmobileapi.jar
-    rm -rf /vendor/app/SmartcardService
     ;;
   *)
     echo "Nothing to do!"
