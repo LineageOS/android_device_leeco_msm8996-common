19e53de4f0d4887c61bdde0d5c861f2974210f69
mosimchah <mosimchah@gmail.com>
2019-09-06 08:18:10 +0200

    msm8996: temp disable overlay-lineage
    Change-Id: I6a2df90a51f4964122a4a8712bc58b6c02b3ef86

--- msm8996.mk
+++ msm8996.mk
@@ -20,8 +20,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
+#    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := framework-res
 
