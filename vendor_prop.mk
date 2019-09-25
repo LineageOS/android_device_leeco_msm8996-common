065c2bbfdef6cbed79b97919f2646313b2fd1ce1
LuK1337 <priv.luk@gmail.com>
2019-09-25 18:42:24 +0200

    msm8996-common: Convert remaining configstore settings to props
    Change-Id: Iadefa31bc863b7ab70239874bed82bef240cbba5

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -162,7 +162,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_phase_offset_ns=1500000
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
 
