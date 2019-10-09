8aa0cb6a36cc323ae2d28e0d1b08632e2fa6d902
Scott Warner <Tortel1210@gmail.com>
2019-10-09 13:03:24 -0400

    msm8996-common: Use qcom-legacy-um sepolicy
    Change-Id: I176a98ee02a803df3e2ffd990c519ec4dba406d3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -220,7 +220,7 @@ TARGET_RIL_VARIANT := caf
 VENDOR_SECURITY_PATCH := 2016-10-01
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
