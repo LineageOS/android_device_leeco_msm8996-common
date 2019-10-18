fd28fa86806ef3fa8683f64e1a4db19c5acdc95e
dianlujitao <dianlujitao@lineageos.org>
2019-10-18 11:05:40 +0800

    msm8996-common: Enable QC framework value-adds
    Change-Id: I1987d3509b3db7cd4204537a6f2dc2ff4c611278

--- msm8996.mk
+++ msm8996.mk
@@ -301,6 +301,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.va_aosp.support=1
+
+PRODUCT_ODM_PROPERTIES += \
+    ro.vendor.qti.va_odm.support=1
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
