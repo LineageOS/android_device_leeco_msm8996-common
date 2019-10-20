61c8afc6d83bdcd6e1c92a4b7c7e66dfe3815412
Ethan Chen <intervigil@gmail.com>
2019-10-20 01:17:32 -0700

    msm8996-common: Remove dead RCS packages
    Change-Id: Ie2c03fa11cb08c7ab900d7b666981d592aba19c7

--- msm8996.mk
+++ msm8996.mk
@@ -340,13 +340,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libjson
 
-# RCS
-PRODUCT_PACKAGES += \
-    rcs_service_aidl \
-    rcs_service_aidl.xml \
-    rcs_service_api \
-    rcs_service_api.xml
-
 # Recovery
 PRODUCT_PACKAGES += \
     librecovery_updater_leeco
