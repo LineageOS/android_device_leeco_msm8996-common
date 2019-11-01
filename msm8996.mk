f035cc1de244a0b1345a231a202fe9e9f2e0004a
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-01 07:19:07 -0500

    msm8996-common: Exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO
    
    Change-Id: Iff826faf1864388066a488acc9682636b98d1098

--- msm8996.mk
+++ msm8996.mk
@@ -24,6 +24,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Init
 PRODUCT_COPY_FILES += \
