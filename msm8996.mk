9c0779646b3a33fae76eae3b5b2dcce49d77c953
Marc Bourgoin <themard85@gmail.com>
2019-11-21 18:02:44 -0700

    msm8996-common: use TARGET_COPY_OUT_SYSTEM for privapp permissions
    Change-Id: I67e31953d9c5ab68908cf16a6bad45057d254ec1

--- msm8996.mk
+++ msm8996.mk
@@ -288,8 +288,8 @@ PRODUCT_COPY_FILES += \
 
 # Qualcomm
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
 # QMI
 PRODUCT_PACKAGES += \
