179811f24b4e488ab2b90e7148e787950c3ad6c7
Michael Bestas <mkbestas@lineageos.org>
2020-01-18 21:30:42 +0200

    msm8996-common: Cleanup audio package list
    Change-Id: Ibaa141fb19035637a7dd4826920f22cebafd9c74

--- msm8996.mk
+++ msm8996.mk
@@ -87,14 +87,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@5.0 \
     android.hardware.audio@5.0-impl \
-    android.hardware.audio.common@5.0 \
-    android.hardware.audio.common@5.0-util \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@5.0 \
     android.hardware.audio.effect@5.0-impl \
     audio.a2dp.default \
     audio.primary.msm8996 \
