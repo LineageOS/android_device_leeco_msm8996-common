d91c89c5c4a4c6abab3a1f3cd2fc6f602d1c0494
Marc Bourgoin <themard85@gmail.com>
2020-03-23 23:49:34 -0600

    msm8996-common: adapt releasetools for Android 10
    * system and vendor now mount in /mnt/ so adapt our scripts to be
      compatible to Android 10's A-only System-as-root implementation.
    
    * LineageOS 17.1 does not include sgdisk for recovery, so provide
      our own copy for Lineage Recovery.
    
    * LOS Recovery uses toybox, not toybox_static
    
    * use the strings command instead of cat for devinfo in deunify
      to ensure we always pull a clean device name.
    
    Change-Id: I98f79eb9ed96bed7adca2127e0ce245d0ab4456b

--- prebuilt/bin/deunify.sh
+++ prebuilt/bin/deunify.sh
@@ -4,28 +4,28 @@
 #
 
 # DEVINFO
-DEVINFO=$(cat /dev/block/sde21)
+DEVINFO=$(strings /dev/block/sde21 | head -n 1)
 
 echo "DEVINFO: ${DEVINFO}"
 
 case "$DEVINFO" in
   le_zl0*)
     # Move firmware
-    mv -f /vendor/firmware/zl0/* /vendor/firmware/
+    mv -f /mnt/vendor/firmware/zl0/* /mnt/vendor/firmware/
 
     # Remove NFC configs
-    rm -f /system/etc/libnfc-nci.conf
-    rm -f /vendor/etc/libnfc-nxp_RF.conf
-    rm -f /vendor/etc/libnfc-nxp.conf
+    rm -f /mnt/system/etc/libnfc-nci.conf
+    rm -f /mnt/vendor/etc/libnfc-nxp_RF.conf
+    rm -f /mnt/vendor/etc/libnfc-nxp.conf
 
     # Remove NFC perms
-    rm -f /vendor/etc/permissions/android.hardware.nfc*
-    rm -f /vendor/etc/permissions/com.android.nfc*
-    rm -f /vendor/etc/permissions/com.nxp.mifare.xml
+    rm -f /mnt/vendor/etc/permissions/android.hardware.nfc*
+    rm -f /mnt/vendor/etc/permissions/com.android.nfc*
+    rm -f /mnt/vendor/etc/permissions/com.nxp.mifare.xml
 
     # Remove NFC services
-    rm -f /vendor/etc/init/android.hardware.nfc*
-    rm -f /vendor/etc/init/vendor.nxp.hardware.nfc*
+    rm -f /mnt/vendor/etc/init/android.hardware.nfc*
+    rm -f /mnt/vendor/etc/init/vendor.nxp.hardware.nfc*
     ;;
   *)
     echo "Nothing to do!"
