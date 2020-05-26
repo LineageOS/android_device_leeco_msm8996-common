e67c15aa5b56ac7abe05846c87fb01505f8bb95f
zhaoguomanong <zhaoguomanong@gmail.com>
2020-05-26 18:39:10 +0800

    msm8996-common: add camera hal level switch in Leeco advanced settings
    Resons to add this switch:
    
      Leeco release it's product only with camera hal1 support,
    they hadn't done any efforts on camera hal3 adaptation, especially the mm-camera and camera driver stuff.
    Although our los can bring it up on camera hal3, but with many issues, such as 4k video snapshot issue,
    zsl capture missing, sometimes taking picture failed, preview shaking, auto flash capture failed...
    The stability of hal1 on x2 is almost the same as stock camera.
    I just add another choice to user to use leeco camera.If they seeking stablity of camera, they can use hal1.
    If they want to use gcam, just use the hal3.
    
    This commit don't means we rollback the camera hal to hal1, just another choice.
    
    Signed-off-by: zhaoguomanong <zhaoguomanong@gmail.com>
    Change-Id: I7d5e0a194e55f456990930d5912e5ca3c6c8211d

--- devicesettings/src/org/lineageos/settings/device/LeecoPreferenceFragment.java
+++ devicesettings/src/org/lineageos/settings/device/LeecoPreferenceFragment.java
@@ -26,11 +26,15 @@ import android.util.Log;
 
 public class LeecoPreferenceFragment extends PreferenceFragment {
 
+    private static final String TAG = "LeecoPreferenceFragment";
+
     private static final String KEY_CAMERA_FOCUS_FIX_ENABLE = "camera_focus_enable";
     private static final String KEY_QUICK_CHARGE_ENABLE = "quick_charge_enable";
+    private static final String KEY_CAMHAL3_ENABLE = "key_camera_hal3_enable";
 
     private SwitchPreference mCameraFocusFixEnable;
     private SwitchPreference mQuickChargeEnable;
+    private SwitchPreference mCamHal3Enable;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -40,33 +44,57 @@ public class LeecoPreferenceFragment extends PreferenceFragment {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        Log.d(TAG, "onCreatePreferences+++");
         addPreferencesFromResource(R.xml.leeco_settings_panel);
         final PreferenceScreen prefSet = getPreferenceScreen();
         mCameraFocusFixEnable = (SwitchPreference) findPreference(KEY_CAMERA_FOCUS_FIX_ENABLE);
         mQuickChargeEnable = (SwitchPreference) findPreference(KEY_QUICK_CHARGE_ENABLE);
+        mCamHal3Enable = (SwitchPreference) findPreference(KEY_CAMHAL3_ENABLE);
+
+        if (SettingsUtils.supportCamHalLevelSwitch()) {
+            mCamHal3Enable.setChecked(SettingsUtils.cameraHAL3Enable());
+            Log.d(TAG, "onCreatePreferences: cam hal3 enable = " + SettingsUtils.cameraHAL3Enable());
+            mCamHal3Enable.setOnPreferenceChangeListener(mPrefListener);
+        } else {
+            prefSet.removePreference(mCamHal3Enable);
+        }
+
 
         if (SettingsUtils.supportsCameraFocusFix()) {
             mCameraFocusFixEnable.setChecked(SettingsUtils.getCameraFocusFixEnabled(getActivity()));
-            mCameraFocusFixEnable.setOnPreferenceChangeListener(PrefListener);
+            mCameraFocusFixEnable.setOnPreferenceChangeListener(mPrefListener);
+            Log.d(TAG, "onCreatePreferences: cam focus fix enable = "
+                    + SettingsUtils.getCameraFocusFixEnabled(getActivity()));
         } else {
             prefSet.removePreference(mCameraFocusFixEnable);
         }
 
         if (SettingsUtils.supportsQuickChargeSwitch()) {
             mQuickChargeEnable.setChecked(SettingsUtils.getQuickChargeEnabled(getActivity()));
-            mQuickChargeEnable.setOnPreferenceChangeListener(PrefListener);
+            mQuickChargeEnable.setOnPreferenceChangeListener(mPrefListener);
+            Log.d(TAG, "onCreatePreferences: quick charge enable = "
+                    + SettingsUtils.getQuickChargeEnabled(getActivity()));
         } else {
             prefSet.removePreference(mQuickChargeEnable);
         }
+        Log.d(TAG, "onCreatePreferences---");
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        Log.d(TAG, "onResume+++");
         getListView().setPadding(0, 0, 0, 0);
+        //prop maybe modify by adb when onpause, so check from prop again
+        if (SettingsUtils.supportCamHalLevelSwitch()
+                && null != mCamHal3Enable) {
+            mCamHal3Enable.setChecked(SettingsUtils.cameraHAL3Enable());
+            Log.d(TAG, "onResume: cam hal3 enable = " + SettingsUtils.cameraHAL3Enable());
+        }
+        Log.d(TAG, "onResume---");
     }
 
-    private Preference.OnPreferenceChangeListener PrefListener =
+    private Preference.OnPreferenceChangeListener mPrefListener =
         new Preference.OnPreferenceChangeListener() {
         @Override
         public boolean onPreferenceChange(Preference preference, Object value) {
@@ -76,10 +104,16 @@ public class LeecoPreferenceFragment extends PreferenceFragment {
                 boolean enabled = (boolean) value;
                 SettingsUtils.setCameraFocusFixEnabled(getActivity(), enabled);
                 SettingsUtils.writeCameraFocusFixSysfs(enabled);
+                Log.d(TAG, "onPreferenceChange: cam focus fix enable = " + enabled);
             } else if (KEY_QUICK_CHARGE_ENABLE.equals(key)) {
                 boolean enabled = (boolean) value;
                 SettingsUtils.setQuickChargeEnabled(getActivity(), enabled);
                 SettingsUtils.writeQuickChargeProp(enabled);
+                Log.d(TAG, "onPreferenceChange: quick charge enable = " + enabled);
+            } else if (KEY_CAMHAL3_ENABLE.equals(key)) {
+                boolean enabled = (boolean) value;
+                SettingsUtils.writeCameraHAL3Prop(enabled);
+                Log.d(TAG, "onPreferenceChange: cam hal3 enable = " + enabled);
             }
 
             return true;
