6180b0e030aaa6895b2901c3f305cd627e50c350
Marc Bourgoin <themard85@gmail.com>
2020-05-28 19:20:38 -0600

    msm8996-common: force camera HAL1 for the following apps
    Facebook, Facebook Messenger, Instagram, Viber and WhatsApp sometimes
    have issues with camera preview freezing with camera HAL3 on our
    devices because the camera blobs were only meant to be used on HAL1,
    and on HAL3 sometimes mm-camera-daemon fails to get the preview frames
    which causes the preview to freeze.
    
    Forcing these apps to use HAL1 prevents this issue from occuring and
    these apps don't require HAL3 for full functionality.
    
    Change-Id: I6926bce825b68f0770c767512372346a479f3ef5

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -49,7 +49,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.HAL3.enabled=1 \
     persist.camera.imglib.fddsp=1 \
     persist.camera.llc=1 \
-    persist.camera.llnoise=1
+    persist.camera.llnoise=1 \
+    vendor.camera.hal1.packagelist=com.facebook.katana,com.facebook.orca,com.instagram.android,com.viber.voip,com.whatsapp
 
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
