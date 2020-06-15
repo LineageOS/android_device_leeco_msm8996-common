71003d67a5abebac01c427eab8bf48aec9b882fc
zhaoguomanong <zhaoguomanong@gmail.com>
2020-06-15 15:49:00 +0800

    msm8996-common: add camera hal level switch in Leeco advanced settings
        Resons to add this switch:
    
          Leeco release it's product only with camera hal1 support,
        they hadn't done any efforts on camera hal3 adaptation, especially the mm-camera and camera driver stuff.
        Although our los can bring it up on camera hal3, but with many issues, such as 4k video snapshot issue,
        zsl capture missing, sometimes taking picture failed, preview shaking, auto flash capture failed...
        The stability of hal1 on x2 is almost the same as stock camera.
        I just add another choice to user to use leeco camera.If they seeking stablity of camera, they can use hal1.
        If they want to use gcam, just use the hal3.
    
        This commit don't means we rollback the camera hal to hal1, just another choice.
    
    Signed-off-by: zhaoguomanong <zhaoguomanong@gmail.com>
    Change-Id: I7d5e0a194e55f456990930d5912e5ca3c6c8211d

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -47,6 +47,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.HAL3.enabled=1 \
+    persist.camera.hal_migrating=0 \
     persist.camera.imglib.fddsp=1 \
     persist.camera.llc=1 \
     persist.camera.llnoise=1 \
