19e79f57a081881c0c9246932cfcfdcdd02d27ed
mosimchah <mosimchah@gmail.com>
2020-06-22 22:07:18 -0400

    msm8996-common: don't force camera hal 1 for whatsapp
    Users are reporting issues with video calls not working  when using hal 1
    
    Change-Id: I1dcd308b5a5bf9123f54bc57f21a40ddc9e50db7

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -50,7 +50,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.imglib.fddsp=1 \
     persist.camera.llc=1 \
     persist.camera.llnoise=1 \
-    vendor.camera.hal1.packagelist=com.facebook.katana,com.facebook.orca,com.instagram.android,com.viber.voip,com.whatsapp
+    vendor.camera.hal1.packagelist=com.facebook.katana,com.facebook.orca,com.instagram.android,com.viber.voip
 
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
