1994b6730c6770e533e4ee87bb0487006fa6edae
LuK1337 <priv.luk@gmail.com>
2020-10-29 14:31:02 +0100

    Revert "msm8996-common: Exclude lineage-sdk overlays from RRO"
    This reverts commit 6a191bbabf6939d98425548b9ffa02c68ce688f2.
    
    Change-Id: I12f150b13686f8b91bb63e4ee728eb4016856aa5

--- msm8996.mk
+++ msm8996.mk
@@ -24,9 +24,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay/packages/apps/Snap
 
 # Permissions
 PRODUCT_COPY_FILES += \
