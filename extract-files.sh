LineageOS/android_device_xiaomi_msm8996-common

msm8996-common: Move telephony packages to /system_ext
Change-Id: I38329f6d4ebb7b1a0b0f01e40918fdb73ccbcba9

extract-files.sh
@@ -33,7 +33,25 @@ source "${HELPER}"
 
 function blob_fixup() {
     case "${1}" in
-    product/lib64/libdpmframework.so)
+    system_ext/etc/init/dpmd.rc)
+        sed -i "s/\/system\/product\/bin\//\/system\/system_ext\/bin\//g" "${2}"
+        ;;
+    system_ext/etc/permissions/com.qti.dpmframework.xml)
+        ;&
+    system_ext/etc/permissions/dpmapi.xml)
+        sed -i "s/\/system\/product\/framework\//\/system\/system_ext\/framework\//g" "${2}"
+        ;;
+    system_ext/etc/permissions/embms.xml)
+        ;&
+    system_ext/etc/permissions/qcrilhook.xml)
+        ;&
+    system_ext/etc/permissions/telephonyservice.xml)
+        sed -i "s/\/system\/framework\//\/system\/system_ext\/framework\//g" "${2}"
+        ;;
+    system_ext/etc/permissions/qti_libpermissions.xml)
+        sed -i "s/name=\"android.hidl.manager-V1.0-java/name=\"android.hidl.manager@1.0-java/g" "${2}"
+        ;;
+    system_ext/lib64/libdpmframework.so)
         sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
         ;;
     vendor/bin/imsrcsd)

LineageOS/android_device_leeco_msm8996-common

extract-files.sh
@@ -71,6 +71,21 @@ fi
 function blob_fixup() {
     case "${1}" in
 
+    # Move telephony packages to /system_ext
+    system_ext/etc/init/dpmd.rc)
+        sed -i "s/\/system\/product\/bin\//\/system\/system_ext\/bin\//g" "${2}"
+        ;;
+
+    # Move telephony packages to /system_ext
+    system_ext/etc/permissions/com.qti.dpmframework.xml|system_ext/etc/permissions/dpmapi.xml|system_ext/etc/permissions/telephonyservice.xml)
+        sed -i "s/\/system\/product\/framework\//\/system\/system_ext\/framework\//g" "${2}"
+        ;;
+
+    # Move telephony packages to /system_ext
+    system_ext/etc/permissions/qcrilhook.xml|)
+        sed -i "s/\/product\/framework\//\/system\/system_ext\/framework\//g" "${2}"
+        ;;
+
     # Provide shim for libdpmframework.so
     product/lib64/libdpmframework.so)
         for  LIBCUTILS_SHIM in $(grep -L "libcutils_shim.so" "${2}"); do