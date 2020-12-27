dd9b960c4e3bbb4ffce75b8aab9f4058c7b58b0e
Michael Bestas <mkbestas@lineageos.org>
2020-12-28 01:17:48 +0200

    msm8996-common: Switch to standalone extract utils
    Align with templates while we are at it
    
    Change-Id: I3cd279191c7bd3582f1c1f80c588eb26cf9da933

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,33 +1,20 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
-INITIAL_COPYRIGHT_YEAR=2016
-
-# Load extractutils and do some sanity checks
-MY_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}/../../.."
-CLEANUP="$1"
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -35,30 +22,27 @@ fi
 source "${HELPER}"
 
 # Initialize the helper for common
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" "true" "${CLEANUP}"
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
-# Copyright headers and guards
+# Warning headers and guards
 write_headers "zl1 x2"
 
-# The standard msm8996-common blobs
+# The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
-# We are done!
+# Finish
 write_footers
 
-# Reinitialize the helper for msm8996-common/${device}
-(
-	INITIAL_COPYRIGHT_YEAR="${DEVICE_BRINGUP_YEAR}"
-	setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEANUP}"
+if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
+    # Reinitialize the helper for device
+    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false
 
-	# Copyright headers and guards
-	write_headers
+    # Warning headers and guards
+    write_headers
 
-	# $1: The device-specific blobs
-	# $2: Make treble compatible paths and put "$(TARGET_COPY_OUT_VENDOR)"
-	#     in generated makefiles
-	write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
+    # The standard device blobs
+    write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
 
-	# We are done!
-	write_footers
-)
+    # Finish
+    write_footers
+fi
